eed17d2a247c0fd82891737641217b3e473279be
Makarand Uparwat <makarand.uparwat@spareshub.com>
2016-02-23 11:40:09 +0530

    another ftp changes pushed via git

--- application/models/Cricket_model.php
+++ application/models/Cricket_model.php
@@ -172,6 +172,127 @@ AND STR_TO_DATE( "'.$next_date.'", "%Y-%m-%d" )', NULL, FALSE );
 		$this->db->insert('cric_user_bet', $data);
 		return $this->db->insert_id();
 	}
+	
+	
+	function cancelCricBet($player_id) {
+        $this->load->library('ion_auth');
+        
+        date_default_timezone_set("Asia/Calcutta");
+        $now = getdate();
+        $now['minutes'] = $now['minutes'] - 1;
+        $minutes = $now['minutes'] - $now['minutes'] % 15;
+        $rounded = $now['year'] . "-" . $now['mon'] . "-" . $now['mday'] . " " . $now['hours'] . ":" . $minutes . ":00";
+        
+        $time = strtotime($rounded);
+        $time = $time + (15 * 60);
+        $date = date("Y-m-d H:i:s", $time);
+        
+        $this->db->select('*');
+        $this->db->from('player_history');
+        $this->db->where('player_id', $player_id);
+        $this->db->where('is_canceled', 1);
+        $this->db->where("timeslot >= '" . $rounded . "' and timeslot <= '" . $date . "' ");
+        $canceled = $this->db->get()->row();
+        if(empty($canceled)){         
+        
+        
+        $this->db->select('sum(bet_amount) as bet_amount,id,transaction_id');
+        $this->db->from('player_history');
+        $this->db->where('player_id', $player_id);
+        
+        $this->db->where("timeslot >= '" . $rounded . "' and timeslot <= '" . $date . "' ");
+        $this->db->order_by('id', 'desc');
+        $this->db->group_by('transaction_id');
+        
+        
+        $query = $this->db->get()->row();
+        $transaction_id = $query->transaction_id;
+        //echo "<pre>";print_r($query->transaction_id);die;
+        
+        if (!empty($query)) {
+            $bet_amount = $query->bet_amount;
+            
+            //calclulate commison and dealer id
+            $this->db->select('dealer_id');
+            $this->db->from('dealer_player');
+            $this->db->where('player_id', $player_id);
+            $query1 = $this->db->get()->row();
+            $dealer_id = $query1->dealer_id;
+            
+            $bet_amount_dealer = $bet_amount * 0.05;
+            
+            $debit = array('id' => 1, 'bet_amount' => $bet_amount - $bet_amount_dealer,);
+            $debit_dealer = array('id' => $dealer_id, 'bet_amount' => $bet_amount_dealer);
+            $credit = array('id' => $player_id, 'bet_amount' => $bet_amount);
+            
+            $this->credit($credit);
+            
+            if (!$this->ion_auth->in_group('demo', $player_id)) {
+                $this->debit($debit);
+                $this->debit_dealer($debit_dealer);
+                 //debit dealers commision
+                
+                //delete admin history and dealer history
+//                $this->db->select('id');
+//                $this->db->from('admin_history');
+//                $this->db->where('player_id', $player_id);
+//                $this->db->order_by('id', 'desc');
+//                $this->db->limit(1);
+//                $query2 = $this->db->get()->row();
+//                $delete_id = $query2->id;
+                $this->db->delete('admin_history', array('transaction_id' => $transaction_id));
+            } 
+            else {
+                
+                $debit_dealer = array('id' => $dealer_id, 'bet_amount' => $bet_amount,);
+                $this->debit_dealer($debit_dealer);
+                 //debit dealers commision
+                
+            }
+            
+            $this->db->delete('player_history', array('transaction_id' => $transaction_id));
+            $this->db->delete('dealer_history', array('transaction_id' => $transaction_id));
+            $this->db->delete('cric_user_bet', array('transaction_id' => $transaction_id));
+            //set is_canceled 1  so that user cannot cancel bet
+             $this->db->set('is_canceled', 1, FALSE);
+             $this->db->where('player_id',$player_id);
+             $this->db->where("timeslot >= '" . $rounded . "' and timeslot <= '" . $date . "' ");
+             $this->db->update('player_history');        
+            return true;
+        } 
+        else {
+            return false;
+        }
+        
+        }else {
+            return false;
+        }
+    }
+	
+	
+	
+	function debit($data) {
+        $this->db->set('present_amount', 'present_amount-' . $data['bet_amount'], FALSE);
+        $this->db->where('id', $data['id']);
+        $this->db->update('user_master');
+    }
+    function debit_dealer($data) {
+        $this->db->set('present_amount', 'present_amount-' . $data['bet_amount'], FALSE);
+        $this->db->where('id', $data['id']);
+        $this->db->update('user_master');
+    }
+    
+    function credit($data) {
+        $this->db->set('present_amount', 'present_amount+' . $data['bet_amount'], FALSE);
+        $this->db->where('id', $data['id']);
+        $this->db->update('user_master');
+    }
+    
+    function credit_dealer($data) {
+        $this->db->set('present_amount', 'present_amount+' . $data['bet_amount'], FALSE);
+        $this->db->where('id', $data['id']);
+        $this->db->update('user_master');
+    }
 
 function checkBettingClosed($match_id,$m_id){
 	$this->db->select('match_id,game_close');
