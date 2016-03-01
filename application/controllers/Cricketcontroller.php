2d0f19996c20ac7d63a183dfdb550df82bf0b589
unknown <shrikantwagh2010@gmail.com>
2016-03-01 16:17:44 +0530

    test live data for access token

--- application/controllers/Cricketcontroller.php
+++ application/controllers/Cricketcontroller.php
@@ -70,8 +70,8 @@ class Cricketcontroller extends CI_Controller {
                 foreach ($UniqueKeyOfMatchArray as $key => $v) {
                     $UniqueKeyOfMatch = $v->unique;
                     $MatchUniqueId = $v->id;
-                    //$MatchUniqueId = 41;
-                    //$UniqueKeyOfMatch = "asiacup_2016_g4";
+                    $MatchUniqueId = 41;
+                    $UniqueKeyOfMatch = "asiacup_2016_g4";
                     //http://www.litzscore.com/rest/v2/
                     $CommonAuthUrl = "https://rest.cricketapi.com/rest/v2/";
                     $url = $CommonAuthUrl."match/" . $UniqueKeyOfMatch . "/?access_token=" . $TokenAccess;
