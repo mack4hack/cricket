fc2eec866b24bda237e2fd48e4d344a2272bc50b
unknown <shrikantwagh2010@gmail.com>
2016-03-02 09:02:45 +0530

    test over summary

--- application/controllers/Cricketcontroller.php
+++ application/controllers/Cricketcontroller.php
@@ -81,7 +81,7 @@ class Cricketcontroller extends CI_Controller {
                     curl_setopt($ch,CURLOPT_ENCODING , "gzip");
                     $output = curl_exec($ch);
                     $LiveMatchArray = json_decode($output);
-                    //echo "ss<pre>";
+                    echo "ss<pre>";
                    //$LiveMatchArray->data->card->balls->$OverBallKeyValue
                      print_r($LiveMatchArray->data); exit;
                     //exit;
