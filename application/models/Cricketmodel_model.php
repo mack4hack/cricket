917e2a21a275db242d490664820763913234002b
unknown <shrikantwagh2010@gmail.com>
2016-03-08 03:27:12 +0530

    Over Summary Api rework

--- application/models/Cricketmodel_model.php
+++ application/models/Cricketmodel_model.php
@@ -193,7 +193,7 @@ class Cricketmodel_model extends CI_Model {
     function GetLiveMatchKeyAPIToday() {
         
         $today = date('Y-m-d');
-        $prv_date = date("Y-m-d", strtotime("-3 days"));
+        $prv_date = date("Y-m-d", strtotime("-4 days"));
         //$next_date = date("Y-m-d", strtotime("+3 days"));
         $this->db->select('*');
         $this->db->from('match_list');
@@ -257,7 +257,7 @@ class Cricketmodel_model extends CI_Model {
     function getMatchList() {
         
         $today = date('Y-m-d');
-        $prv_date = date("Y-m-d", strtotime("-4 days"));
+        $prv_date = date("Y-m-d", strtotime("-2 days"));
         $next_date = date("Y-m-d", strtotime("+5 days"));
         $this->db->select('*');
         $this->db->from('match_list');
